360ff969b88fa8424d779fac8ac82bf656f10b90
Plumtree3D <ham.in.kneesocks@gmail.com>
2022-03-28 13:04:57 +0200

    error page

---
+++ 404.php
@@ -0,0 +1,17 @@
+<?php 
+$title = "Page not found -";
+require 'includes/head.php';
+?>
+<?php include 'includes/switches.html' ?>
+
+<div class="sectionTitle" id="404"> </div>
+<div class="policy">
+    <p class="mainText" id="notFound"> </p>
+</div>
+
+
+
+<script src="script.js"> </script>
+</body>
+
+</html>
